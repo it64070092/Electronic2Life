38a590436d311ede4bc2ea9c9c21c642c4937720
bossserx1337 <150757086+bossserx1337@users.noreply.github.com>
2024-02-28 02:48:31 +0700

    backend update

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,10 +1,72 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('./models/user'); // Check the path
+const Product = require('./models/product');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Connect to MongoDB Atlas
+mongoose.connect('mongodb+srv://bossserx:test123456@electronic2life.rlv0qo2.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Define a simple route
 app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+  res.send('Hello MongoDB Atlas with Express.js!');
+});
 
-app.listen(3000, () => {
-  console.log('Start server at port 3000.')
-})
+// Register a new user
+app.post('/register', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser); // 201 Created status code
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+app.post('/post-product', async (req, res) => {
+  console.log(req.body)
+  try {
+    const { name, price, description } = req.body;
+
+    const newProduct = new Product({
+      name,
+      price,
+      description,
+    });
+
+    const savedProduct = await newProduct.save();
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ backEnd/models/user.js
@@ -0,0 +1,33 @@
+// models/user.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Hash the password before saving it
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 10);
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
