00a4fc241c11d2ba586ef1614b9a11830324762d
NinEAynGNa-64070012 <88431946+NinEAynGNa-64070012@users.noreply.github.com>
2024-03-12 00:11:35 +0700

    Product List and Product Detail
    เชื่อมต่อกับ back ได้ และ จากหน้า Product List ไปหา Product Detail และ แสดงข้อมูลได้แล้ว

--- frontEnd/src/content/ProductDetail.jsx
+++ frontEnd/src/content/ProductDetail.jsx
@@ -1,47 +1,70 @@
-import { useState } from 'react'
-import '../App.css'
-import { useParams } from 'react-router-dom'
-import { ComplexNavbar } from '../components/NavBar'
+import { useEffect, useState } from "react";
+import backendip from "../../backendip";
+import "../App.css";
+import { useParams } from "react-router-dom";
+import { ComplexNavbar } from "../components/NavBar";
+import axios from "axios";
 
 function ProductDetail() {
-  const [count, setCount] = useState(0)
-  const { productId } = useParams()
+  const [Product, setProduct] = useState([]);
+  const { productId } = useParams();
+
+  useEffect(() => {
+    axios
+      .get("http://" + backendip + ":3000/get-product/" + productId)
+      .then((response) => {
+        console.log(response.data.product);
+        setProduct(response.data.product);
+        // console.log(product);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
+
   return (
     <>
-      <ComplexNavbar/>
-      <div className='flex flex-row w-screen h-screen items-center'>
-        <div class='flex flex-col w-[60%] h-[60%] justify-center items-center ml-96 bg-gray-400 rounded-3xl'>
+      <ComplexNavbar />
+      <div class="flex flex-row items-center justify-center h-screen px-9">
+        <div class="flex flex-col w-3/5 h-3/5 justify-center items-center bg-gray-400 rounded-3xl ml-30">
           <img
-            src='https://www.electrolux.co.th/globalassets/catalog/washing-machines--washer-dryers/th-ewf7524d3wb-fr-cl-1500x1500.png'
-            alt='product'
-            class='object-contain w-full h-full rounded-3xl'
+            src="https://www.electrolux.co.th/globalassets/catalog/washing-machines--washer-dryers/th-ewf7524d3wb-fr-cl-1500x1500.png"
+            alt="product"
+            class="object-contain w-full h-full rounded-3xl"
           />
         </div>
-        <div className='flex flex-col h-1/2 justify-center ml-20 gap-6'>
-          <div className='flex flex-row w-1/2 justify-start'>
-            <h1 className='text-4xl'>Product Name {productId}</h1>
+        <div class="flex flex-col w-1/2 h-3/5 justify-center ml-20 gap-6">
+          <div class="flex flex-row w-full justify-start">
+            <h1 class="text-4xl">Product Name {Product.name}</h1>
           </div>
-          <div className='flex flex-col w-1/2 h-full px-6 py-6 justify-evenly bg-gray-200 rounded-3xl border-2 border-solid border-black'>
+          <div class="flex flex-col w-5/6 h-full px-6 py-6 justify-evenly bg-gray-200 rounded-3xl border-2 border-solid border-black">
             <p>Description</p>
-            <p className='px-2 py-2'>Import Material Design Icons, FontAwesome, Jam Icons, EmojiOne, Twitter Emoji and many other icons (more than 100 icon sets containing over 100,000 icons) to Figma document as vector shapes.</p>
+            <p class="px-2 py-2">
+              {Product.description}
+            </p>
             <p>ประเภท</p>
-            <p className='px-2 py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. A architecto repellendus reprehenderit laborum dignissimos velit totam aperiam molestias quo. Aspernatur accusantium eligendi dolorem maxime harum fuga suscipit atque quod rerum.</p>
-            <div className='flex flex-row justify-between'>
-              <div className='px-2 py-2'>
-                <p className='text-2xl'>ราคา: 9999 บาท</p>
+            <p class="px-2 py-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A
+              architecto repellendus reprehenderit laborum dignissimos velit
+              totam aperiam molestias quo. Aspernatur accusantium eligendi
+              dolorem maxime harum fuga suscipit atque quod rerum.
+            </p>
+            <div class="flex flex-row justify-between">
+              <div class="px-2 py-2">
+                <p class="text-2xl">${Product.price}</p>
               </div>
-              <div className='px-2 py-2'>
-                <p className='text-2xl'>รีวิว: 4.5/5</p>
+              <div class="px-2 py-2">
+                <p class="text-2xl">รีวิว: 4.5/5</p>
               </div>
             </div>
           </div>
-          <button class='w-1/2 bg-blue-500 text-white px-6 py-3 rounded-lg'>Buy Now</button>
+          <button class="w-5/6 bg-emerald-500 text-white px-6 py-3 rounded-lg hover:bg-emerald-700">
+            Buy Now
+          </button>
         </div>
-
-
       </div>
     </>
-  )
+  );
 }
 
-export default ProductDetail
+export default ProductDetail;
--- frontEnd/src/content/ProductPage.jsx
+++ frontEnd/src/content/ProductPage.jsx
@@ -1,13 +1,107 @@
-import { useState } from 'react'
-import '../App.css'
+import { useEffect, useState } from "react";
+import backendip from "../../backendip";
+import "../App.css";
+import { ComplexNavbar } from "../components/NavBar";
+import { Square3Stack3DIcon } from "@heroicons/react/24/solid";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function ProductPage() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("http://" + backendip + ":3000/get-products")
+      .then((response) => {
+        console.log(response.data.products);
+        setProducts(response.data.products);
+        // console.log(products);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
+
+  function ToProductDetail(productId) {
+    navigate(`/product/${productId}`);
+  }
 
   return (
-    <h1>ProductPage</h1>
-     
-  )
+    <>
+      <ComplexNavbar />
+      <section>
+        {/* <div class="flex flex-row mx-10 my-10"> */}
+        <div class="flex flex-col md:flex-row mx-2 md:mx-10 my-10">
+          {/* Left side content */}
+          {/* <div class="flex flex-col w-1/3 h-[50%] pl-4 pt-4 bg-white rounded-lg shadow-lg overflow-hidden"> */}
+          <div class="flex flex-col h-[70%] w-full md:w-1/3 bg-white rounded-lg shadow-lg overflow-hidden mb-4 md:mb-0">
+            <div class="p-3">
+              <h2 class="text-lg font-semibold text-gray-800 mb-2">Product</h2>
+              <div class="flex items-center mb-4">
+                <input
+                  type="text"
+                  class="w-full px-3 py-1 border border-gray-300 rounded-md focus:outline-none focus:border-black"
+                />
+              </div>
+              <div class="mb-9">
+                <label class="flex items-center">
+                  <input
+                    type="checkbox"
+                    class="form-checkbox h-5 w-5 text-blue-500 my-2"
+                  />
+                  <span class="ml-2 text-gray-700">Laptop</span>
+                </label>
+                <label class="flex items-center">
+                  <input
+                    type="checkbox"
+                    class="form-checkbox h-5 w-5 text-blue-500 my-2"
+                  />
+                  <span class="ml-2 text-gray-700">Phone</span>
+                </label>
+              </div>
+            </div>
+          </div>
+
+          {/* Right side content */}
+          {/* <div class="grid grid-cols-4 gap-4 mx-8 "> */}
+          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mx-2 md:mx-8 ">
+            {products.map((product) => (
+              <div class="h-full w-52 mr-6 bg-white shadow-md rounded-lg overflow-hidden">
+                <div onClick={() => ToProductDetail(product._id)} class="top flex justify-center w-full h-48">
+                  <img
+                    class="w-full object-cover transition duration-300 transform hover:scale-105"
+                    src={product.productImage}
+                    alt="Image"
+                  />
+                </div>
+                <div class="p-4">
+                  <p class="text-gray-800 text-sm truncate">{product.name}</p>
+                  <h class="text-gray-600 text-sm truncate">
+                    {product.description}
+                  </h>
+                  <div class="flex justify-between mt-1">
+                    <h class="text-gray-800 text-base font-medium truncate">
+                      ${product.price}
+                    </h>
+                    <h class="text-green-500 text-sm truncate mt-1">in stock</h>
+                  </div>
+                  <div class="flex justify-between mt-2">
+                    <button
+                      class="bg-black text-sm hover:bg-gray-700 text-white py-0 px-4 rounded"
+                    >
+                      + Add To Cart
+                    </button>
+                    <Square3Stack3DIcon class="h-5 w-5 text-red-500 mr-2" />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default ProductPage
+export default ProductPage;
